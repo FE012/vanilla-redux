b618a52eed9fd60202fe48ccee895474af93f928
FE012 <twte96@gmail.com>
2022-12-24 18:13:23 +0900

    count button 완료

--- src/index.js
+++ src/index.js
@@ -4,10 +4,39 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  return state;
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
 const countStore = createStore(countModifier);
 
-console.log(countStore.getState());
+const onChange = () => {
+  number.textContent = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+// console.log(countStore);
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
